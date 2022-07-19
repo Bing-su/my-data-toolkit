7fa5b7c41ff7b45069b617e562d3d1d944304984
dowon <ks2515@naver.com>
2022-07-19 16:35:50 +0900

    fit: if not exists

--- dwkit/web_data_corpus/web_data_corpus.py
+++ dwkit/web_data_corpus/web_data_corpus.py
@@ -51,9 +51,10 @@ class WebDataCorpus:
         self.output = Path(output)
         self.output.parent.mkdir(parents=True, exist_ok=True)
 
-        with self.output.open("w", encoding="utf-8"):
-            # make file for `async_open`
-            pass
+        if not self.output.exists():
+            with self.output.open("w", encoding="utf-8"):
+                # make file for `async_open`
+                pass
 
         if target in ("라벨링", "원천"):
             self.target = target
